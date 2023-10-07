761150c326508001f159138a6961e16dd129704b
umid <umidshrofov@gmail.com>
2023-10-07 12:34:26 +0500

    carousel

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,60 @@
+import { Button } from '../Generic'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { EffectFade } from 'swiper/modules';
+
+
+// Import Swiper styles
+import 'swiper/css';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// img
+import Img1 from '../../assets/images/carusle IMG1.png'
+import Img2 from '../../assets/images/carusle IMG2.png'
+
+
+const Carousel = () => {
+    return (
+        <div className='Container'>
+            <div className='Carousel'>
+                <Swiper
+                    modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+                    spaceBetween={80}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    scrollbar={{ draggable: true }}
+                    className='Swper'
+
+                >
+
+                    <SwiperSlide>
+                        <div className='Carousle_title'>
+                            <h1>{`Skyper Pool Partment`}</h1>
+                            <Button >Read more</Button>
+                        </div>
+                        <div className='Carousle_blur'>
+                        </div>
+                        <img src={Img1} alt="carousel" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className='Carousle_title'>
+                            <h1>{`Vermont Farmhouse With Antique Jail I the Week's Most Popular Home`}</h1>
+                        </div>
+                        <div className='Carousle_blur'>
+                        </div>
+                        <img src={Img2} alt="carousel" />
+                    </SwiperSlide>
+
+                </Swiper>
+
+            </div>
+        </div>
+    )
+}
+export default Carousel
